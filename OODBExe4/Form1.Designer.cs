027a8864b799d941459afb7b00a1e2268843c396
WangXinwei <WangXinwei@DESKTOP-6VHU3KN>
2016-10-02 21:24:28 +0800

    Add project files.

---
+++ OODBExe4/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace OODBExe4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Location = new System.Drawing.Point(153, 295);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 129);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(246, 43);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(143, 39);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Find";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(122, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 44);
+            this.textBox4.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(682, 295);
+            this.button8.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(69, 35);
+            this.button8.TabIndex = 28;
+            this.button8.Text = ">>";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(28, 295);
+            this.button7.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(69, 35);
+            this.button7.TabIndex = 27;
+            this.button7.Text = "<<";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(712, 250);
+            this.button6.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(39, 35);
+            this.button6.TabIndex = 26;
+            this.button6.Text = ">";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 250);
+            this.button5.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(39, 35);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "<";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(558, 244);
+            this.button4.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 47);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(399, 244);
+            this.button3.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 47);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 244);
+            this.button2.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 47);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(292, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 44);
+            this.textBox3.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(383, 44);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(292, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 44);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 37);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Rating";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 37);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "MovieTitle";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 37);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "VideoCode";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 47);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(501, 141);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(127, 47);
+            this.button10.TabIndex = 30;
+            this.button10.Text = "Create";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Visible = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 528);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ OODBExe4/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OODBExe4
+{
+    public partial class Form1 : Form
+    {
+        List<Movy> list;
+        int posn;
+        DafestyEntities context;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void display()
+        {
+            textBox1.Text = list[posn].VideoCode.ToString();
+            textBox2.Text = list[posn].MovieTitle.ToString();
+            textBox3.Text = list[posn].Rating.ToString();
+        }
+
+        public void retrieve()
+        {
+            list[posn].VideoCode = Convert.ToInt16(textBox1.Text);
+            list[posn].MovieTitle = textBox2.Text;
+            list[posn].Rating = textBox3.Text;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Movy mv;
+            mv = list[posn];
+            list.Remove(mv);
+            context.Movies.Remove(mv);
+            int i = context.SaveChanges();
+            if (i > 0)
+            {
+                MessageBox.Show("Successful!");
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//"Load"
+        {
+            posn = 0;
+            context = new DafestyEntities();
+            var q = from x in context.Movies select x;
+            list = q.ToList();
+            display();
+        }
+
+        private void button6_Click(object sender, EventArgs e)//">"
+        {
+            if (posn < list.Count - 1)
+            {
+                posn++;
+                display();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (posn > 0)
+            {
+                posn--;
+                display();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            posn = list.Count - 1;
+            display();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            posn = 0;
+            display();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int searchCode = Convert.ToInt16(textBox4.Text);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (searchCode == list[i].VideoCode)
+                {
+                    posn = i;
+                }
+            }
+            display();
+        }
+
+        private void button2_Click(object sender, EventArgs e)//"Update"
+        {
+            retrieve();
+            int i = context.SaveChanges();
+            if (i > 0)
+            {
+                MessageBox.Show("Successful!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            button10.Visible = true;
+            //context.SaveChanges();
+            //var q = from x in context.Movies select x;
+            //list = q.ToList();
+
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Movy mv = new Movy();
+            mv.VideoCode = Convert.ToInt16(textBox1.Text);
+            mv.MovieTitle = textBox2.Text;
+            mv.Rating = textBox3.Text;
+            context.Movies.Add(mv);
+            list.Add(mv);
+            int i = context.SaveChanges();
+            if (i > 0)
+            {
+                MessageBox.Show("Successful!");
+                posn = list.Count - 1;
+            }
+            button10.Visible = false;
+        }
+    }
+}
